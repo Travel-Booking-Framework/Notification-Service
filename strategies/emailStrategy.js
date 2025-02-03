c8069c13fb2c78520765fbc513647248ebe4e39f
poorah <pouyarahmaneian@gmail.com>
2025-02-04 00:55:38 +0330

    bug fixed!

--- strategies/emailStrategy.js
+++ strategies/emailStrategy.js
@@ -9,14 +9,3 @@ class EmailStrategy extends NotificationStrategy {
 }
 
 module.exports = new EmailStrategy();
-const NotificationStrategy = require("./notificationStrategy");
-const transporter = require("../config/emailConfig");
-
-class EmailStrategy extends NotificationStrategy {
-  async send(to, subject, text) {
-    await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text });
-    console.log(`Email sent to ${to}`);
-  }
-}
-
-module.exports = new EmailStrategy();
